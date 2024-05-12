702f1023a5bef240eb68204930a1b4536e42eb42
YousufStat <stat.yousuf.bsmrstu@gmail.com>
2024-05-12 15:45:49 +0600

    Edit something

--- src/data/data.js
+++ src/data/data.js
@@ -1,27 +1,52 @@
 import WorkIm from '../assets/projects/Capture2.PNG'
 import RealEst from '../assets/projects/Capture.PNG'
 import MetoMoni from '../assets/projects/Capture6.PNG'
+import Tailwind from '../assets/projects/cap-1.png'
+import BookReader from '../assets/projects/cap-2.png'
+import AmarShop from '../assets/projects/cap-3.png'
+
 export const data=[
     {
         id:1,
         name:"React JS Application",
         image:WorkIm,
         github:"https://github.com/YousufStat/My-React-Project",
-        live:"",
+        live:"https://yousuf-react-app-c6tb.vercel.app/",
     },
     {
         id:2,
         name:"React JS Application",
         image:RealEst,
-        github:"https://github.com/YousufStat/My_Portfolio_website",
+        github:"https://github.com/YousufStat/React_Portfolio",
         live:"",
     },
     {
         id:3,
+        name:"React+Tailwind Application",
+        image:Tailwind,
+        github:"https://github.com/YousufStat/Tailwind-Projects",
+        live:"https://tailwind-projects-two.vercel.app/",
+    },
+    {
+        id:4,
+        name:"React Book Finder Application",
+        image:BookReader,
+        github:"https://github.com/YousufStat/React-Assingment-1",
+        live:"https://react-assingment-1.vercel.app/",
+    },
+    {
+        id:5,
         name:"React JS Application",
         image:MetoMoni,
-        github:"",
-        live:"",
+        github:"https://github.com/YousufStat/Sunnah-Marriage-App",
+        live:"https://sunnah-marriage-app.vercel.app/home",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:AmarShop,
+        github:"https://github.com/YousufStat/Amar-shop-ecommerce",
+        live:"https://hilarious-beignet-51181f.netlify.app/",
     },
 
 ]
